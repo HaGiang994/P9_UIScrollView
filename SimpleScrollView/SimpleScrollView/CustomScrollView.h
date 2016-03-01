6b0ed3fb09b7dd536f70e93d9c1cb8685f34c669
HaGiang994 <gianglehongha2805@gmail.com>
2016-03-01 17:44:38 +0700

    Add source code
    This is file SimpleScrollView

---
+++ SimpleScrollView/SimpleScrollView/CustomScrollView.h
@@ -0,0 +1,15 @@
+//
+//  CustomScrollView.h
+//  SimpleScrollView
+//
+//  Created by MacPro1 on 3/1/16.
+//  Copyright © 2016 MacPro1. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+@interface CustomScrollView : UIView
+
+@property (nonatomic) CGSize contentSize;
+
+@end
